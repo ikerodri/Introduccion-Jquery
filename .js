21c41b7bb10e02e7bcec324842e97a86c72e1a3b
ikerodri <39056860+ikerodri@users.noreply.github.com>
2018-12-07 23:34:25 +0100

    Create .js

---
+++ .js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+//Exercici 1.1 Seleccionar tots els elements div que posseeixen la classe module.
+
+//alert($("div").length)
+
+//Exercici 1.2 Especificar 3 seleccions que puguin seleccionar el tercer ítem de la llista desordenada #myList. Quin és el millor per utilitzar? Per què?
+
+//$("#mylist li:nth-child(3)")
+//$("#myListItem")
+//$("#mylist").children("li").first().next().next()
+
+//Exercici 1.3 Seleccionar l'element label de l'element input utilitzant un selector d'atribut. (ajuda: el selector d’atribut són els label[x=”y”])
+
+//$('label[for="q"]')
+
+//Exercici 1.4 Esbrinar quants elements en la pàgina estan ocults (ajuda: .length)
+
+
+
+//Exercici 1.5 Esbrinar quantes imatges a la pàgina posseeixen l'atribut alt.
+
+//alert($("img[alt]").length)
+
+//Exercici 1.6 Seleccionar totes les files senars del cos de la taula.
+
+//alert($("tr:nth-child(odd)").length)
+
+//Exercici 2.1 Seleccionar totes les imatges a la pàgina; registrar a la consola l'atribut src de cada imatge.
+    
+
+    
+//Exercici 2.2 Seleccionar l'element input, després dirigir-se cap el formulari i afegir-hi una classe a aquest.
+
+//Exercici 2.3 Seleccionar l'ítem que posseeix la classe current dins de la llista #myList i eliminar aquesta classe en l'element; després afegir la classe current al següent ítem de la llista.
+
+//Exercici 2.4 Seleccionar l'element select dins de #specials; després (des de l’element), dirigir-se cap el botó submit.
+
+//Exercici 2.5 Seleccionar el primer ítem de la llista en l'element #slideshow; afegir-hi la classe current al mateix i després afegir la classe disabled als elements germans.
+})
